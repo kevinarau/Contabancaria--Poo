ec9694b075280dadb0e714a59585b481e6594570
kevin <kevin.aparecido@gmail.com>
2024-07-31 19:09:25 -0300

    Meu Primeiro Commit

---
+++ src/Contas/SistemaContas.java
@@ -0,0 +1,12 @@
+package Contas;
+
+public interface SistemaContas  {
+
+    public void depositar(double deposito);
+    public  void sacar(double sacar);
+    public  void tranferencia( double valor, ContaBancaria destino);
+
+
+
+
+}
